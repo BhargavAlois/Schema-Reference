6691b94aa4edbe2272a9ba8a99235538b69241cb
BhargavAlois <bhargav.akino@hotmail.com>
2025-02-04 11:02:46 +0530

    Create TemplateBankUploadLayout.js

---
+++ TemplateBankUploadLayout.js
@@ -0,0 +1,20 @@
+layout: [
+            {
+              type: 'section',
+              classNames: 'd-flex flex-row',
+              fields: ['template_image',
+                {
+                  type: 'section',
+                  classNames: 'd-flex flex-col',
+                  fields: ['placeholder']
+                },
+                'name',
+                {
+                  type: 'section',
+                  classNames: 'd-flex flex-col',
+                  fields: ['description'],
+                },
+                'fontSize'
+              ]
+            }
+          ],
